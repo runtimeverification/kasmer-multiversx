06c002a1bd14769f95008c4d8039b71a1e594359
Virgil <virgil.serbanuta@runtimeverification.com>
2023-10-02 20:20:49 +0300

    Testing

--- kmxwasm/src/tests/integration/test_integration.py
+++ kmxwasm/src/tests/integration/test_integration.py
@@ -12,8 +12,8 @@ from kmxwasm.json import load_json_kclaim
 from kmxwasm.property_testing.wasm_krun_initializer import WasmKrunInitializer
 from kmxwasm.property_testing.running import Success, run_claim
 
-TEST_DATA = ROOT / 'kmxwasm' / 'src' / 'tests' / 'data'
-INPUT_FILES = TEST_DATA.glob('*.json')
+TEST_DATA = (Path(__file__).parent / 'data').resolve(strict=True)
+INPUT_FILES = [TEST_DATA / 'test_call_add_less-spec.json']
 
 @pytest.fixture(scope='module')
 def tools() -> Tools:
@@ -21,7 +21,7 @@ def tools() -> Tools:
                 output_dir=KBUILD_DIR, config_file=KBUILD_ML_PATH, target=HASKELL, booster=False
             )
 
-@pytest.mark.parametrize('test_file', TEST_DATA, ids=[str(test_file.relative_to(TEST_DATA)) for test_file in TEST_DATA])
+@pytest.mark.parametrize('test_file', INPUT_FILES, ids=[str(test_file.relative_to(TEST_DATA)) for test_file in INPUT_FILES])
 def test_success(test_file:Path, tools:Tools) -> None:
     claim = load_json_kclaim(test_file)
     # Fix the claim, it's not clear why these cells are being
@@ -34,6 +34,7 @@ def test_success(test_file:Path, tools:Tools) -> None:
         claim=claim,
         restart_kcfg=None,
         depth=1000,
+        run_id = None,
     )
 
     assert isinstance(result, Success), [result]
