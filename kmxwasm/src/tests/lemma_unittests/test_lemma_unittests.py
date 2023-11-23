bd59374a21594cd20b7a1b62c3e237bd1c683e13
Virgil <virgil.serbanuta@runtimeverification.com>
2023-11-23 20:03:08 +0200

    Unittests for lemmas

---
+++ kmxwasm/src/tests/lemma_unittests/test_lemma_unittests.py
@@ -0,0 +1,15 @@
+from pathlib import Path
+
+import pytest
+
+from kmxwasm.testing.fixtures import Tools
+
+K_SRC_DIR = Path(__file__).parent.parent.parent.parent / 'k-src'
+LEMMA_PROOFS_DIR = K_SRC_DIR / 'lemmas' / 'tests'
+PROVE_FILES = tuple(LEMMA_PROOFS_DIR.rglob('*.k'))
+PROVE_TEST_DATA = tuple(((str(input_path.relative_to(K_SRC_DIR)), input_path)) for input_path in PROVE_FILES)
+
+
+@pytest.mark.parametrize(('test_id', 'test_file'), PROVE_TEST_DATA, ids=[test_id for test_id, _ in PROVE_TEST_DATA])
+def test_success(test_id: str, test_file: Path, lemma_tests_tools: Tools) -> None:
+    lemma_tests_tools.kprove.prove(test_file)
