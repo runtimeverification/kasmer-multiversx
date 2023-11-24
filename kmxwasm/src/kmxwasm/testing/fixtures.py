c6abc2b73179f093e674e68d8d9eb395f7665832
Georgy Lukyanov <mail@geo2a.info>
2023-11-24 11:58:17 +0100

    Add --bug-report CLI option and thread BugReport argument through (#51)

--- kmxwasm/src/kmxwasm/testing/fixtures.py
+++ kmxwasm/src/kmxwasm/testing/fixtures.py
@@ -9,5 +9,7 @@ from ..tools import Tools
 @pytest.fixture(scope='session')
 def tools(tmp_path_factory: TempPathFactory) -> Tools:
     build_path = tmp_path_factory.mktemp('kbuild')
-    tools = kbuild_semantics(output_dir=build_path, config_file=KBUILD_ML_PATH, target=HASKELL, booster=True)
+    tools = kbuild_semantics(
+        output_dir=build_path, config_file=KBUILD_ML_PATH, target=HASKELL, booster=True, bug_report=None
+    )
     return tools
