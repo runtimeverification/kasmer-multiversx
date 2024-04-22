8edd14e71f2c6bdd53e9c9c500007eebdc0f12be
Virgil <virgil.serbanuta@runtimeverification.com>
2024-04-22 17:02:19 +0300

    Split lemma tests into regression and unit tests

--- kmxwasm/src/tests/lemma_tests/test_lemmas.py
+++ kmxwasm/src/tests/lemma_tests/test_lemmas.py
@@ -7,12 +7,27 @@ from pyk.prelude.ml import mlOr
 from kmxwasm.testing.fixtures import Tools
 
 CURRENT_DIR = Path(__file__).parent
-LEMMA_PROOFS_DIR = CURRENT_DIR / 'tests'
-PROVE_FILES = tuple(LEMMA_PROOFS_DIR.rglob('*.k'))
-PROVE_TEST_DATA = tuple(((str(input_path.relative_to(CURRENT_DIR)), input_path)) for input_path in PROVE_FILES)
+
+LEMMA_UNITTEST_DIR = CURRENT_DIR / 'unit_tests'
+UNITTEST_FILES = tuple(LEMMA_UNITTEST_DIR.rglob('*.k'))
+UNIT_TEST_DATA = tuple(((str(input_path.relative_to(CURRENT_DIR)), input_path)) for input_path in UNITTEST_FILES)
+
+LEMMA_REGRESSION_DIR = CURRENT_DIR / 'regression_tests'
+REGRESSION_FILES = tuple(LEMMA_REGRESSION_DIR.rglob('*.k'))
+REGRESSION_TEST_DATA = tuple(
+    ((str(input_path.relative_to(CURRENT_DIR)), input_path)) for input_path in REGRESSION_FILES
+)
 
 
-@pytest.mark.parametrize(('test_id', 'test_file'), PROVE_TEST_DATA, ids=[test_id for test_id, _ in PROVE_TEST_DATA])
-def test_success(test_id: str, test_file: Path, lemma_tests_tools: Tools) -> None:
+@pytest.mark.parametrize(('test_id', 'test_file'), UNIT_TEST_DATA, ids=[test_id for test_id, _ in UNIT_TEST_DATA])
+def test_unit(test_id: str, test_file: Path, lemma_tests_tools: Tools) -> None:
+    result = lemma_tests_tools.kprove.prove(test_file)
+    assert CTerm._is_top(mlOr([res.kast for res in result]))
+
+
+@pytest.mark.parametrize(
+    ('test_id', 'test_file'), REGRESSION_TEST_DATA, ids=[test_id for test_id, _ in REGRESSION_TEST_DATA]
+)
+def test_regression(test_id: str, test_file: Path, lemma_tests_tools: Tools) -> None:
     result = lemma_tests_tools.kprove.prove(test_file)
     assert CTerm._is_top(mlOr([res.kast for res in result]))
