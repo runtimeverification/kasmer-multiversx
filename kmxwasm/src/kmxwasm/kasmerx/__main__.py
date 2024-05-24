a22a3edf62ce1f8f7acbf8202bf8bf59d558d3a8
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-05-24 08:59:59 +0000

    Add CLI skeleton

---
+++ kmxwasm/src/kmxwasm/kasmerx/__main__.py
@@ -0,0 +1,66 @@
+from __future__ import annotations
+
+from argparse import ArgumentParser
+from dataclasses import dataclass
+from pathlib import Path
+from typing import TYPE_CHECKING
+
+from pyk.utils import check_dir_path
+
+if TYPE_CHECKING:
+    from collections.abc import Sequence
+
+
+@dataclass
+class KasmerxOpts:
+    project_dir: Path
+
+    def __init__(self, *, project_dir: Path):
+        check_dir_path(project_dir)
+        self.project_dir = project_dir.resolve()
+
+
+class BuildOpts(KasmerxOpts): ...
+
+
+def main() -> None:
+    import sys
+
+    kasmerx(sys.argv[1:])
+
+
+def kasmerx(args: Sequence[str]) -> None:
+    opts = _parse_args(args)
+    match opts:
+        case BuildOpts():
+            return _kasmerx_build(opts)
+        case _:
+            raise AssertionError()
+
+
+def _kasmerx_build(opts: BuildOpts) -> None: ...
+
+
+def _parse_args(args: Sequence[str]) -> KasmerxOpts:
+    ns = _arg_parser().parse_args(args)
+
+    project_dir = Path(ns.directory)
+
+    return {
+        'build': BuildOpts(project_dir=project_dir),
+    }[ns.command]
+
+
+def _arg_parser() -> ArgumentParser:
+    parser = ArgumentParser(prog='kasmerx')
+    parser.add_argument('-C', '--directory', default='.', help='path to test contract (default: CWD)')
+
+    command_parser = parser.add_subparsers(dest='command', required=True)
+
+    command_parser.add_parser('build', help='build main and test contracts')
+
+    return parser
+
+
+if __name__ == '__main__':
+    main()
