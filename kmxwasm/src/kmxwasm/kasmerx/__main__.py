dd657d14817801a7b6fd1c217d06bb9147ced688
Tamás Tóth <tothtamas28@users.noreply.github.com>
2024-05-28 11:42:57 +0000

    Add further command line options

--- kmxwasm/src/kmxwasm/kasmerx/__main__.py
+++ kmxwasm/src/kmxwasm/kasmerx/__main__.py
@@ -6,6 +6,7 @@ from dataclasses import dataclass
 from pathlib import Path
 from typing import TYPE_CHECKING
 
+import pyk
 from pyk.utils import check_dir_path
 
 from .build import kasmerx_build
@@ -16,6 +17,8 @@ from .verify import kasmerx_verify
 if TYPE_CHECKING:
     from collections.abc import Sequence
     from typing import Final
+
+    from pyk.utils import BugReport
 
 
 logger: Final = logging.getLogger(__name__)
@@ -39,6 +42,11 @@ class FuzzOpts(KasmerxOpts): ...
 @dataclass
 class VerifyOpts(KasmerxOpts):
     test: str
+    step: int | None
+    iterations: int | None
+    restart: bool | None
+    booster: bool | None
+    bug_report: BugReport | None
 
 
 def main() -> None:
@@ -79,7 +87,15 @@ def _kasmerx_fuzz(opts: FuzzOpts) -> None:
 
 def _kasmerx_verify(opts: VerifyOpts) -> None:
     project = load_project(opts.project_dir)
-    kasmerx_verify(project, opts.test)
+    kasmerx_verify(
+        project=project,
+        test=opts.test,
+        step=opts.step,
+        iterations=opts.iterations,
+        restart=opts.restart,
+        booster=opts.booster,
+        bug_report=opts.bug_report,
+    )
 
 
 # ----------------------
@@ -95,7 +111,15 @@ def _parse_args(args: Sequence[str]) -> KasmerxOpts:
     return {
         'build': BuildOpts(project_dir=project_dir),
         'fuzz': FuzzOpts(project_dir=project_dir),
-        'verify': VerifyOpts(project_dir=project_dir, test=ns.test),
+        'verify': VerifyOpts(
+            project_dir=project_dir,
+            test=ns.test,
+            step=ns.step,
+            iterations=ns.iterations,
+            restart=ns.restart,
+            booster=ns.booster,
+            bug_report=ns.bug_report,
+        ),
     }[ns.command]
 
 
@@ -110,6 +134,33 @@ def _arg_parser() -> ArgumentParser:
 
     verify_parser = command_parser.add_parser('verify', help='verify test contract')
     verify_parser.add_argument('test', metavar='TEST', help='name of test function')
+    verify_parser.add_argument(
+        '--restart',
+        action='store_true',
+        default=None,
+        help='restart the backend with the results of the previous run',
+    )
+    verify_parser.add_argument(
+        '--step',
+        type=int,
+        help='number of steps to run at a time',
+    )
+    verify_parser.add_argument(
+        '--iterations',
+        type=int,
+        help='number of batches of --step steps to run',
+    )
+    verify_parser.add_argument(
+        '--booster',
+        action='store_true',
+        default=None,
+        help='use the booster backend',
+    )
+    verify_parser.add_argument(
+        '--bug-report',
+        type=pyk.cli.args.bug_report_arg,
+        help='generate bug report with given name',
+    )
 
     return parser
 
