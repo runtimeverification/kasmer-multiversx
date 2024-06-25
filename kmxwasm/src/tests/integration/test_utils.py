cf99bce907b3449e0cee88ed8b45ae4af34ff69c
Burak Bilge Yalcinkaya <bbyalcinkaya@gmail.com>
2024-06-25 14:00:09 +0300

    add tests

---
+++ kmxwasm/src/tests/integration/test_utils.py
@@ -0,0 +1,17 @@
+import subprocess
+from pathlib import Path
+
+import pytest
+
+from kmxwasm.kasmer.utils import load_project
+
+ROOT = Path(subprocess.check_output(['git', 'rev-parse', '--show-toplevel']).decode().strip())
+TEST_CONTRACTS_DIR = ROOT / 'tests' / 'contracts'
+
+PROJECT_DIRS = [i for i in TEST_CONTRACTS_DIR.iterdir() if i.is_dir()]
+
+
+@pytest.mark.parametrize('project_dir', PROJECT_DIRS, ids=str)
+def test_load_project(project_dir: Path):
+
+    load_project(project_dir)
--- kmxwasm/src/tests/unit/test_unit.py
+++ kmxwasm/src/tests/unit/test_unit.py
@@ -1,5 +1,54 @@
-from kmxwasm.hello import hello
+from pathlib import Path
+
+import pytest
+from pyk.utils import abs_or_rel_to
+
+from kmxwasm.kasmer.utils import read_contract_target
+
+TEST_DATA = (
+    (
+        'string-single-contract',
+        '../../deps/mx-sdk-rs/contracts/examples/adder',
+        '../../deps/mx-sdk-rs/contracts/examples/adder',
+        None,
+    ),
+    (
+        'dict-single-contract',
+        {
+            'path': '../../deps/mx-sdk-rs/contracts/examples/adder',
+        },
+        '../../deps/mx-sdk-rs/contracts/examples/adder',
+        None,
+    ),
+    (
+        'string-multi-contract',
+        '../../deps/mx-sdk-rs/contracts/examples/multisig/output/multisig.wasm',
+        '../../deps/mx-sdk-rs/contracts/examples/multisig',
+        'multisig',
+    ),
+    (
+        'dict-multi-contract',
+        {
+            'path': '../../deps/mx-sdk-rs/contracts/examples/multisig',
+            'name': 'multisig-full',
+        },
+        '../../deps/mx-sdk-rs/contracts/examples/multisig',
+        'multisig-full',
+    ),
+)
 
 
-def test_hello() -> None:
-    assert hello('World') == 'Hello, World!'
+@pytest.mark.parametrize(('id', 'input', 'directory', 'name'), TEST_DATA, ids=[td[0] for td in TEST_DATA])
+def test_read_contract_target(id: str, input: str | dict, directory: str, name: str) -> None:
+
+    # Given
+    project_dir = Path('a/b/c')
+
+    # When
+    actual = read_contract_target(project_dir, input)
+
+    # Then
+    actual_path = actual.directory.resolve()
+    expected_path = abs_or_rel_to(Path(directory), base=project_dir).resolve()
+    assert actual_path == expected_path
+    assert actual.name == name
